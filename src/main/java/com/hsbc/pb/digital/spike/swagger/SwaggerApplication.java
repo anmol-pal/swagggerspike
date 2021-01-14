d14b109036eed5cf8e1df11ebc365b7074f6cc5e
Anmol Pal <anmolpal1110@gmail.com>
2021-01-14 14:24:08 +0530

    initial springboot

---
+++ src/main/java/com/hsbc/pb/digital/spike/swagger/SwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.hsbc.pb.digital.spike.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsbc/pb/digital/spike/swagger/SwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsbc.pb.digital.spike.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
